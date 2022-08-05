ac85640acad8f5a8c610cda1a2b8733a26e75735
e-baron <raphael.baroni@vinci.be>
2022-08-05 12:52:36 +0200

    update :  api step1 refactored and changed to 'basic'

--- backend-restful-api/express-static-file-server/index.js
+++ backend-restful-api/express-static-file-server/index.js
@@ -1,12 +1,10 @@
-// Create an Express app
-var express = require("express");
-var app = express();
-// Express Middleware to serve static files from ./public directory
-app.use(express.static("public"));
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
 
 const PORT = 7777;
 
-// Start an HTTP server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- backend-restful-api/express-static-file-server/public/index.js
+++ backend-restful-api/express-static-file-server/public/index.js
@@ -1,37 +1,10 @@
-const RECT_NUMBER = 101;
+const body = document.querySelector("body");
 
-let myCanvas = document.querySelector("canvas");
-let myContext = myCanvas.getContext("2d");
-let page = document.querySelector("#page");
-// set the canvas dimensions
-let pageWidth = page.clientWidth;
-let pageHeight = page.clientHeight;
-myCanvas.width = pageWidth - 20;
-myCanvas.height = pageHeight;
+body.addEventListener("click", startOrStopSound);
 
-// call the callback to draw our animation when the browser is ready
-requestAnimationFrame(drawOneFrame);
+function startOrStopSound() {
+  const myAudioPlayer = document.querySelector("#audioPlayer");
 
-function drawOneFrame() {
-  // Reset everything done in the previous frame
-  // We could force the width or height of canvas to force a redraw myCanvas.width = pageWidth;myCanvas.height = pageHeight;
-  // however that would not be optimized.
-  myContext.clearRect(0, 0, pageWidth, pageHeight);
-
-  myContext.fillStyle = "blue"; //'rgba(255,0,0,0.5)';//'blue';
-
-  //draw dynamically the rectangles at random locations
-  for (let i = 0; i < RECT_NUMBER; i++) {
-    myContext.fillRect(
-      Math.floor(Math.random() * pageWidth),
-      Math.floor(Math.random() * pageHeight),
-      20,
-      20
-    );
-  }
-  // Refresh automatically the animation via this recursive call :
-  requestAnimationFrame(drawOneFrame);
-
-  // Slow the animation down via setTimeout
-  //requestAnimationFrame(() => setTimeout(drawOneFrame,1000));
+  if (myAudioPlayer.paused) myAudioPlayer.play();
+  else myAudioPlayer.pause();
 }
